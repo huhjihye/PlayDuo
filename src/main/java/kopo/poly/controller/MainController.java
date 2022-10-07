e2cfc54cce0d13e789db90cd061ecc6efedb1b24
floramiss <floramiss@naver.com>
2022-10-07 14:41:52 +0900

    플레듀오 MainController 수정 완료

--- src/main/java/kopo/poly/controller/MainController.java
+++ src/main/java/kopo/poly/controller/MainController.java
@@ -8,11 +8,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class MainController {
 
+    private String alt_title = "";
+    private String alt_state = "";
+    private String msg = "";
+    private String url = "";
+    private int res = 0;
+
+
+    //동영상페이지를 보여줌
+    @GetMapping(value = "welcome")
+    public String welcome() {
+        log.info(this.getClass().getName() + "메인페이지 시작 ");
+        return "welcome";
+    }
+
     //메인페이지를 보여줌
     @GetMapping(value = "mainPage")
     public String main() {
         log.info(this.getClass().getName() + "메인페이지 시작 ");
         return "main/mainPage";
+    }
+
+    //메인페이지허지혜를 보여줌
+    @GetMapping(value = "mainPage2")
+    public String main2() {
+        log.info(this.getClass().getName() + "메인페이지 시작 ");
+        return "main/mainPage2";
     }
 
     //마이페이지를 보여줌
@@ -22,9 +43,18 @@ public class MainController {
         return "main/myPage";
     }
 
+
+    //학습 선택를 보여줌
+    @GetMapping(value="studySelect")
+    public String studySelect(){
+        log.info(this.getClass().getName() + "마이페이지 시작 ");
+        return "main/studySelect";
+    }
+
     @GetMapping(value = "gameSelect")
     public String gameSelect(){
         log.info(this.getClass().getName()+"게임 선택 페이지 시작!");
         return "main/gameSelect";
     }
+
 }
